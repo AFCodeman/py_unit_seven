2778b8dc3887fe757ee52648e5d978e791eecea5
anthonyfagiolo <fagioan@ssfs.org>
2021-11-09 09:54:42 -0500

    started with strings

--- strings_one.py
+++ strings_one.py
@@ -1,9 +1,14 @@
 def half_slice(word):
-    pass
+    midpoint = len(word)//2
+    half1 = word[:midpoint]
+    half2 = word[midpoint:]
+    return(half2+half1)
 
 
 def no_first_last(str):
-    pass
+    nofirst=str[1:]
+    nolast=str[:-1]
+    return nofirst,nolast
 
 
 def longest(phrase):
--- upper_lower.py
+++ upper_lower.py
@@ -1,7 +1,16 @@
 
 
 def main():
-    pass
+    word = str(input("Feed me word. "))
+    printingLetters(word)
+
+def upperLower(word):
+    print(word.upper())
+    print(word.lower())
+
+def printingLetters(word):
+    for x in word:
+        print(x)
 
 if __name__ == '__main__':
     main()

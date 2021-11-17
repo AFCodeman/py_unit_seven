e3d4406e3ed553165b58b34460f54a351938c0a1
anthonyfagiolo <fagioan@ssfs.org>
2021-11-17 08:59:45 -0500

    finished assignment seven

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,31 +1,39 @@
 import math
 
 alpha = str("abcdefghijklmnopqrstuvwxyz")
-list = []
 
 def main():
     opt = str(input("Press e to encode, press d to decode, and press q to quit."))
     word= str(input("Enter text to be encoded or decoded: "))
     key = int(input("Assign a value to the key:"))
     if opt == "e":
-        print(encode(key, word))
+        print(encode(key,word))
     elif opt == "d":
-        print(decode(key, word))
+        print(decode(key,word))
     elif opt=="q":
         print("Your encryption has finished. Good luck!")
 
 
-def encode(key, word):
-    long = len(word.index())
-    for letter in word(len(long)):
-        new = int(alpha.index([letter+key])
-        list.append(alpha[new])
-    return list
+def encode(word, key):
+    alpha = str("abcdefghijklmnopqrstuvwxyz")
+    blank = ""
+    for letter in word:
+        numb = alpha.index(letter)
+        newNumb = (numb + key)%26
+        blank += alpha[newNumb]
+    return blank
+
 
 
 
 def decode (key, word):
-    pass
+    alpha = str("abcdefghijklmnopqrstuvwxyz")
+    blank = ""
+    for letter in word:
+        numb = alpha.index(letter)
+        newNumb = (numb - key) % 26
+        blank += alpha[newNumb]
+    return blank
 
 
 
